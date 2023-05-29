70c97281e867d56059733c08dd07f65dff8a8c9a
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-05-29 18:20:22 +0100

    ratings created

--- src/main/java/com/parjalRai/films/model/Film.java
+++ src/main/java/com/parjalRai/films/model/Film.java
@@ -6,11 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-import java.awt.*;
 import java.util.List;
 
 @Document(collection = "ghibli")
