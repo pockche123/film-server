8cdb53d6f6dce9af2ccce97e9853dd12b41e7dd8
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-06-14 18:02:35 +0100

    create fixed

--- src/main/java/com/parjalRai/films/controller/CommentController.java
+++ src/main/java/com/parjalRai/films/controller/CommentController.java
@@ -1,16 +1,23 @@
 package com.parjalRai.films.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.parjalRai.films.exception.NotFoundException;
 import com.parjalRai.films.model.Comment;
 import com.parjalRai.films.model.dto.CommentDTO;
 import com.parjalRai.films.service.CommentService;
@@ -20,6 +27,7 @@ import com.parjalRai.films.service.CommentService;
 @CrossOrigin(origins = "http://localhost:3000")
 public class CommentController {
     
+    private static final Optional<ObjectId> Optional = null;
     @Autowired
     private CommentService commentService;
     
@@ -40,8 +48,41 @@ public class CommentController {
 
     @PostMapping
     public ResponseEntity<Comment> createComment(@RequestBody CommentDTO commentDTO) {
-        
+        try {
+
+            
+            Comment comment = commentService.createComment(commentDTO.text(), commentDTO.discussionId(),
+                    commentDTO.commentId(), commentDTO.username(), commentDTO.likes());
+            return ResponseEntity.ok(comment);
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteComment(@PathVariable ObjectId id) {
+        boolean deleted = commentService.deleteComment(id);
+        if(deleted){
+            return ResponseEntity.ok("Comment deleted successfully");
+        } else 
+
+            return ResponseEntity.notFound().build();
+        }
+        
+    @PatchMapping("/{id}")
+    public ResponseEntity<Comment> updateComment(@RequestBody Comment comment, @PathVariable ObjectId id){
+        Comment updatedComment = commentService.updateCommentDetails(comment, id);
+        if(updatedComment != null){
+            return ResponseEntity.ok(updatedComment);
+        } else{
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }     
+
+    
+       }
 
 
-}
+
+
+
