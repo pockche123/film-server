456f51dea35bbdb311b319ab89671bbeed3855a7
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-06-19 15:54:37 +0100

    update and delete methods created

--- src/main/java/com/parjalRai/films/controller/DiscussionController.java
+++ src/main/java/com/parjalRai/films/controller/DiscussionController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.PatchExchange;
 
 import com.parjalRai.films.exception.NotFoundException;
 import com.parjalRai.films.model.Discussion;
@@ -26,9 +26,8 @@ import com.parjalRai.films.service.DiscussionService;
 @RequestMapping("api/v1/discussion")
 @CrossOrigin(origins = "http://localhost:3000")
 public class DiscussionController {
-    
 
-    @Autowired 
+    @Autowired
     private DiscussionService discussionService;
 
     @GetMapping
@@ -41,12 +40,10 @@ public class DiscussionController {
         return ResponseEntity.ok(discussionService.getDiscussionsByFilm(filmTitle));
     }
 
-
     @GetMapping("/user/{username}")
     public ResponseEntity<List<Discussion>> getDiscussionsByUser(@PathVariable String username) {
         return ResponseEntity.ok(discussionService.getDiscussionsByUser(username));
     }
-
 
     @PostMapping
     public ResponseEntity<Discussion> createDiscussion(@RequestBody DiscussionDTO discussionDTO) {
@@ -60,20 +57,36 @@ public class DiscussionController {
 
         }
     }
-    
+
     @PatchMapping("/{id}")
-    public ResponseEntity<Discussion> updateDiscussion(@RequestBody Discussion discussion, @PathVariable ObjectId id, 
+    public ResponseEntity<Discussion> updateDiscussion(@RequestBody Discussion discussion, @PathVariable ObjectId id,
             Authentication authentication) {
-          Discussion updatedDiscussion = discussionService.updateDiscussionDetails(discussion, id); 
-          
-          String username = authentication.getName();
-          if (updatedDiscussion != null && discussionService.isTheOwner(id, username)) {
-              return ResponseEntity.ok(updatedDiscussion);
-          } else {
-              return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-                }
+        Discussion updatedDiscussion = discussionService.updateDiscussionDetails(discussion, id);
 
-
-        
+        String username = authentication.getName();
+        if (updatedDiscussion != null && discussionService.isTheOwner(id, username)) {
+            return ResponseEntity.ok(updatedDiscussion);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteDiscussion(@PathVariable ObjectId id, Authentication authentication) {
+        String username = authentication.getName();
+
+        if (discussionService.isTheOwner(id, username)) {
+            boolean deleted = discussionService.deleteDiscussion(id);
+            if (deleted) {
+                return ResponseEntity.ok("Discussion deleted successfully");
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
 }
