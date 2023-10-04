75b51ac851832ed10a4f3c72aefe50ac3154cc47
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-10-04 18:13:27 +0100

    favourite done

--- src/main/java/com/parjalRai/films/repository/FavouriteRepository.java
+++ src/main/java/com/parjalRai/films/repository/FavouriteRepository.java
@@ -1,12 +1,21 @@
 package com.parjalRai.films.repository;
 
+import java.util.List;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.parjalRai.films.model.Favourite;
+import com.parjalRai.films.model.Film;
+import com.parjalRai.films.model.UserEntity;
 
 public interface FavouriteRepository extends MongoRepository<Favourite, ObjectId> {
 
+
+
+    List<Favourite> findByUserEntity(UserEntity userEntity);
     
-    
+
+    boolean existsByUserEntityAndFilm(UserEntity user, Film film);
+
 }
